90e3fab0fb216a6bfca904f775231eaa4f9c0b48
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-02-17 16:30:13 +0100

    _

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :events, through: :attendances
   has_many :admin_events, foreign_key: 'admin_id', class_name: "Event"
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,5 @@ module Eventbrite
     # the framework and any gems in your application.
   end
 end
+
+
