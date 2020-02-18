0bc139159fc51f1516036afcc688b487f89820e7
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-02-18 15:51:32 +0100

    index front

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,30 @@
 
 
 User.destroy_all
+Event.destroy_all
 
 10.times do
   User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     description: Faker::Lorem.paragraph(sentence_count: 3),
-    email: Faker::Name.first_name.downcase + "@yopmail.com"
+    email: Faker::Name.first_name.downcase + "@yopmail.com",
+    password: "mdpmdp"
   )
+end
+
+
+multiple_of_five = [5, 10, 15, 20, 25, 30, 35, 40]
+a=1
+20.times do
+  Event.create!(
+    start_date: Faker::Time.between(from: DateTime.now, to: DateTime.now+30),
+    duration: multiple_of_five.sample,
+    title: "Fake Event " + a.to_s,
+    description: Faker::Lorem.paragraph,
+    price: Faker::Number.between(from: 20, to: 1000),
+    location: Faker::Address.city,
+    admin_id: User.all.sample.id
+  )
+a+=1
 end
