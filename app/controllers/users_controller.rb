3ddef2e89b48b3d952325c3779217a586a8e1f89
Raphael Chappert <raphael.chappert@hotmail.fr>
2020-02-18 17:38:10 +0100

    current user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  
   def show
+    signedinuserprofile
+    @events = current_user.events
+  end
+
+  def signedinuserprofile
+    profile = User.find(current_user.id)
+    if profile.nil?
+      redirect_to "/profile/new"
+    end
   end
 end
